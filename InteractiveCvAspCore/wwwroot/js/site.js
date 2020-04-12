7cdda0ee608afe871adaada5e87d35a6a6e2bf95
Mvdb-hub <mvdbroucke@outlook.be>
2020-04-12 17:31:00 +0200

    Smooth scrolling function (JQ) and CSS cleanup

--- InteractiveCvAspCore/wwwroot/js/site.js
+++ InteractiveCvAspCore/wwwroot/js/site.js
@@ -8,6 +8,30 @@ $(document).ready(function ()
         $('.navbar-nav .nav-item').removeClass('active');
         $(this).parent("li").addClass('active');
     });
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+            var offsetTop = $(hash).offset().top
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: offsetTop
+            }, 500, function () {
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        }
+    });
 });
 
 
@@ -18,9 +42,8 @@ window.onscroll = function () {
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
-function MakeNavbarSticky()
-{
-    if (window.pageYOffset > sticky -60) {
+function MakeNavbarSticky() {
+    if (window.pageYOffset >= sticky) {
         navbar.classList.add("fixed-top");
     } else {
         navbar.classList.remove("fixed-top");
